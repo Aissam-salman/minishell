42bc0ccdeea2a13085b7c77eee562ddeb13d0dc8
Timothe Bras <tibras@f4r3s7.clusters.42paris.fr>
2026-02-05 12:53:32 +0100

    Ajout LIBFT

---
+++ libft/includes/libft.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tibras <tibras@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 18:19:51 by tibras            #+#    #+#             */
+/*   Updated: 2026/01/27 09:50:46 by tibras           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+// Standard headers
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdarg.h>
+
+// Include all subdirectory headers
+# include "ft_char.h"
+# include "ft_strings.h"
+# include "ft_conversion.h"
+# include "ft_memory.h"
+# include "ft_list.h"
+# include "ft_gnl.h"
+# include "ft_output.h"
+# include "sorting.h"
+# include "ft_gc.h"
+
+
+#endif
