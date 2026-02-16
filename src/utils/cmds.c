95fd3c968fbedde917db8aee6d22fbf7593cbd2e
Timothe Bras <tibras@f6r2s15.clusters.42paris.fr>
2026-02-16 12:02:59 +0100

    :D

--- src/utils/cmds.c
+++ src/utils/cmds.c
@@ -6,7 +6,7 @@
 /*   By: tibras <tibras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 17:34:00 by tibras            #+#    #+#             */
-/*   Updated: 2026/02/14 18:36:10 by tibras           ###   ########.fr       */
+/*   Updated: 2026/02/14 18:36:26 by tibras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,6 +107,7 @@ int	ft_token_affect(t_minishell *minishell, t_cmd *cmd, t_token **token_ptr, int
 	{
 		if (!next || !next->str || !next->str[0])
 			return (ft_error(SYNTAX_ERROR, "Syntax error near unexpected token 'newline'", NULL));
+
 		// A MODIFIER : VALEUR DE RETOUR
 		if (ft_redirection_handler(minishell, cmd, token))
 			return (GENERAL_ERROR);
