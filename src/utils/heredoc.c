b111041834e875283564ffc692b00571422c1431
Aissam Lamjadab <alamjada@f6r3s13.clusters.42paris.fr>
2026-02-21 19:43:21 +0100

    start norme heredoc

--- src/utils/heredoc.c
+++ src/utils/heredoc.c
@@ -6,10 +6,11 @@
 /*   By: tibras <tibras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 15:04:28 by tibras            #+#    #+#             */
-/*   Updated: 2026/02/20 12:38:46 by tibras           ###   ########.fr       */
+/*   Updated: 2026/02/21 19:42:39 by alamjada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "errors.h"
 #include "minishell.h"
 
 void ft_signal_heredoc_handler(int sig)
@@ -20,7 +21,24 @@ void ft_signal_heredoc_handler(int sig)
 	close(STDIN_FILENO);
 }
 
-// A MODIFIER : RAJOUTER LA GESTION DES SIGNAUX
+int ft_signal_here_ctrld(int saved_stdin, t_token *token)
+{
+	dup2(saved_stdin, STDIN_FILENO);
+	close(saved_stdin);
+	ft_putstr_fd("minishell: warning: here-document delimited by end-of-file (wanted `", 2);
+	ft_putstr_fd(token->str, 2);
+	ft_putstr_fd("')\n", 2);
+	return (SUCCESS);
+}
+
+int ft_signal_here_ctrlc(int saved_stdin, char *line)
+{
+	dup2(saved_stdin, STDIN_FILENO);
+	close(saved_stdin);
+	free(line);
+	return (SUCCESS);
+}
+
 void	ft_heredoc(t_minishell *minishell, t_cmd *cmd, t_token *token, int mod)
 {
 	char	*line;
@@ -30,7 +48,6 @@ void	ft_heredoc(t_minishell *minishell, t_cmd *cmd, t_token *token, int mod)
 	int 	saved_stdin;
 
 	limiter_len = ft_strlen(token->str);
-	// ON SECURISE L'OUVERTUR DU PIPE
 	if (pipe(pipefd))
 	{
 		ft_error(NULL, GENERAL_ERROR, "Heredoc error :",
@@ -39,49 +56,32 @@ void	ft_heredoc(t_minishell *minishell, t_cmd *cmd, t_token *token, int mod)
 	}
 	saved_stdin = dup(STDIN_FILENO);
 	signal(SIGINT, ft_signal_heredoc_handler);
-	// BOUCLE DE READ
 	while (1)
 	{
-		// RECUPERE LA LINE
 		line = readline("> ");
-		//Ctrl-C
-		if (g_signal_glob == SIGINT)
-		{
-			dup2(saved_stdin, STDIN_FILENO);
-			close(saved_stdin);
-			free(line);
-			break;
-		}
-		//TODO: add error same shell ctrl-D
-		if (!line )
-		{
-			dup2(saved_stdin, STDIN_FILENO);
-			close(saved_stdin);
-			ft_putstr_fd("minishell: warning: here-document delimited by end-of-file (wanted `", 2);
-			ft_putstr_fd(token->str, 2);
-			ft_putstr_fd("')\n", 2);
+		if (g_signal_glob == SIGINT && ft_signal_here_ctrlc(saved_stdin, line))
 			break ;
-		}
+		if (!line && ft_signal_here_ctrld(saved_stdin, token))
+			break ;
 		else
 			ft_gc_add_node(&minishell->gc, line);
 		line_len = ft_strlen(line);
-		// SI HERE_DOC TROUVÉ
 		if (!ft_strncmp(line, token->str, limiter_len)
 			&& line_len == limiter_len)
 			break ;
-		// IMPRIME LA LINE DANS PIPE
 		ft_putstr_fd(line, pipefd[1]);
 		ft_putchar_fd('\n', pipefd[1]);
 		line = NULL;
 	}
 	close(saved_stdin);
+	//pipefd, minishell, mod, cmd
 	close(pipefd[1]);
-	// SI DERNIER HERE_DOC DE LA CHAINE
 	if (g_signal_glob == SIGINT)
 	{
 		close(pipefd[0]);
 		minishell->exit_status = 130;
 	}
+	// SI DERNIER HERE_DOC DE LA CHAINE
 	if (mod == 1)
 		ft_redirection_exec(pipefd[0], &cmd->infd);
 	else
@@ -97,14 +97,9 @@ t_token	*ft_heredoc_find_last(t_token *token)
 	while (token && token->type != PIPE)
 	{
 		if (token->type == IN_DCHEVRON)
-		{
-			// ft_printf("TEST HEREDOC : %s\n", token->next->str);
 			last = token;
-		}
 		token = token->next;
 	}
-	// if (last)
-	// 	ft_printf("%s\n", last->next->str);
 	return (last);
 }
 
@@ -112,10 +107,7 @@ void	ft_heredoc_handle(t_minishell *minishell, t_cmd *cmd, t_token *token)
 {
 	t_token	*last;
 
-	// TROUVER LE DERNIER HEREDOC
 	last = ft_heredoc_find_last(token);
-	// while (token && token->next->type != PIPE)
-	// {
 	if (token == last)
 		ft_heredoc(minishell, cmd, token->next, 1);
 	else
--- src/utils/parser_utils.c
+++ src/utils/parser_utils.c
@@ -6,57 +6,54 @@
 /*   By: alamjada <alamjada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 13:04:05 by tibras            #+#    #+#             */
-/*   Updated: 2026/02/21 19:09:00 by alamjada         ###   ########.fr       */
+/*   Updated: 2026/02/21 19:29:23 by alamjada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	ft_expend_separator(t_minishell *minishell, char *usable_str, char *buffer)
+{
+	if (ft_buffer_add(usable_str, '$') != 0)
+		return (ft_error(minishell, BUFFER_FAIL, "Insufficient buffer size",
+				NULL));
+	if (ft_strlcat(usable_str, buffer, BUFFER_SIZE) > BUFFER_SIZE)
+		return (ft_error(minishell, BUFFER_FAIL, "Insufficient buffer size",
+				NULL));
+	return (SUCCESS);
+}
+
+int	ft_expend_error_val(t_minishell *minishell, char *usable_str, int *start)
+{
+	char	*err_value;
+
+	err_value = ft_itoa_gc(minishell->cached_status, &minishell->gc);
+	if (!err_value)
+		return (ft_error(minishell, MALLOC_FAIL, "Error malloc expands", NULL));
+	if (ft_strlcat(usable_str, err_value, BUFFER_SIZE) > BUFFER_SIZE)
+		return (ft_error(minishell, BUFFER_FAIL, "Insufficient buffer size",
+				NULL));
+	(*start)++;
+	return (SUCCESS);
+}
+
 int	ft_expend(char *str, int *start, char *usable_str, t_minishell *minishell)
 {
 	char	buffer[BUFFER_SIZE];
-	char	*err_value;
 	t_env	*path_env;
 
-	// char *expension;
 	ft_bzero(buffer, BUFFER_SIZE);
-	// ON INCREMENTE POUR NE PAS PRENDRE LE $
-	// ATTENTION A L'EXPAND POUR LA VALEUR DE RETOUR
-	if (ft_ischarset(str[*start + 1],SEPARATORS) || !str[*start + 1])
-	{
-		// CA C'EST GOOD
-		(*start)++;
-		if (ft_buffer_add(usable_str, '$') != 0)
-			return (ft_error(minishell, BUFFER_FAIL, "Insufficient buffer size",
-					NULL));
-		if (ft_strlcat(usable_str, buffer, BUFFER_SIZE) > BUFFER_SIZE)
-			return (ft_error(minishell, BUFFER_FAIL, "Insufficient buffer size",
-					NULL));
-		return (0);
-	}
+	if (ft_ischarset(str[*start + 1], SEPARATORS) || !str[*start + 1])
+		return ((*start)++, ft_expend_separator(minishell, usable_str, buffer));
 	(*start)++;
 	if (str[*start] && str[*start] == '?')
-	{
-		err_value = ft_itoa_gc(minishell->cached_status, &minishell->gc);
-		if (!err_value)
-			return (ft_error(minishell, MALLOC_FAIL, "Error malloc expands",
-					NULL));
-		// SI TAILLE DU BUFFER TROP PETITE
-		if (ft_strlcat(usable_str, err_value, BUFFER_SIZE) > BUFFER_SIZE)
-			return (ft_error(minishell, BUFFER_FAIL, "Insufficient buffer size",
-					NULL));
-		(*start)++;
-		return (0);
-	}
-
-	// ON PARCOURT STR JUSQU'A " ou ' ou $ ou SPACES
+		return (ft_expend_error_val(minishell, usable_str, start));
 	while (str[*start] && !ft_ischarset(str[*start], SEPARATORS))
 	{
 		if (ft_buffer_add(buffer, str[*start]) != 0)
 			return (BUFFER_FAIL);
 		(*start)++;
 	}
-	// ON RECUPERE L'ENV
 	path_env = ft_env_find(minishell->head_env, buffer);
 	if (path_env && path_env->content)
 	{
@@ -65,34 +62,26 @@ int	ft_expend(char *str, int *start, char *usable_str, t_minishell *minishell)
 			return (ft_error(minishell, BUFFER_FAIL, "Insufficient buffer size",
 					NULL));
 	}
-	return (0);
+	return (SUCCESS);
 }
 
-// FONCTION PRINCIPALE POUR GERE LES QUOTES
-void	ft_quotes_handle(t_minishell *minishell, t_token *token)
+void	ft_quotes_utils(t_token *token, t_minishell *minishell,
+		char *usable_str)
 {
-	int		i;
-	char	usable_str[BUFFER_SIZE];
+	int	i;
 
 	i = 0;
-	minishell->state = NORMAL;
-	ft_bzero(usable_str, BUFFER_SIZE);
-	// ON BOUCLE SUR CHAQUE ELEMENT DE TOKEN->STR
 	while (token->str[i])
 	{
-		// ON AFFECTE L'ETAT ET ON GARDE LE PRECEDENT EN MEMOIRE
 		ft_state_detect(token->str[i], minishell);
-
-		// SI ON TROUVE UN DOLLAR
 		if (token->str[i] == '$' && minishell->state != IN_QUOTE)
 		{
 			if (ft_expend(token->str, &i, usable_str, minishell))
 				return ;
 			continue ;
 		}
-		// CONDITIONS POUR RAJOUTER MALGRE LES STATES OU LES SEPARATORS (A JOLIFIER)
 		else if ((token->str[i] != '\'' || minishell->state == IN_DQUOTE)
-				&& (token->str[i] != '\"' || minishell->state == IN_QUOTE))
+			&& (token->str[i] != '\"' || minishell->state == IN_QUOTE))
 		{
 			if (ft_buffer_add(usable_str, token->str[i]))
 			{
@@ -102,7 +91,16 @@ void	ft_quotes_handle(t_minishell *minishell, t_token *token)
 		}
 		i++;
 	}
-	// DUP DU BUFFER POUR REMPLACER STR DU TOKEN AVEC LES EXPENDS SI NECESSAIRE
+}
+
+// FONCTION PRINCIPALE POUR GERE LES QUOTES
+void	ft_quotes_handle(t_minishell *minishell, t_token *token)
+{
+	char	usable_str[BUFFER_SIZE];
+
+	minishell->state = NORMAL;
+	ft_bzero(usable_str, BUFFER_SIZE);
+	ft_quotes_utils(token, minishell, usable_str);
 	token->str = ft_strdup_gc(usable_str, &minishell->gc);
 	if (!token->str)
 		ft_error(minishell, MALLOC_FAIL, "Error malloc end of quotes handling",
